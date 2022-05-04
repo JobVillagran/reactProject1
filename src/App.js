04f6ad4c50b9296bee974e36cb16c0919089f6a8
jobvillagran <jobvillagran@gmail.com>
2022-05-04 08:14:23 -0600

    work 04-05-22

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  
+  const repApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);    //Aca le estamos diciendo, la data que vamos a utilizar del json que viene de la API
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        {/* INICIO TERNARIOS - Nos permitira ver si el objeto character viene vacio y realizar una funcion especifica */}
+        {characters ? (
+          <Characters character={characters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={repApi} className="btn-search">
+              Ver todos los personajes
+            </button>
+          </>
+        )}
+        {/* FIN TERNARIOS */}
       </header>
     </div>
   );
