e4d1fc1c008db611a9695e670e731ccd5bc124ba
jobvillagran <jobvillagran@gmail.com>
2022-05-04 10:05:14 -0600

    work characters status 04-05-22

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -10,7 +10,25 @@ export default function Characters(props) {
             <div className="container-characters">
                 {characters.map((character, index) => (
                     <div className="character-container" key={index}>
-                        <p>{character.name}</p>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                    <span className="alive" />
+                                    Alive
+                                    </>
+                                ) : (
+                                    <>
+                                    <span className="dead" />
+                                    Dead
+                                    </>
+                                )}
+                            </h6>
+                        </div>
                     </div>
                 ))}
             </div>
